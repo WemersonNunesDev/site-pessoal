fdf6b11687188c785424b13d1e8b3d8e03ef11ff
WemersonNunesDev <wemersononunes@gmail.com>
2025-08-11 18:09:32 -0300

    feat: Adicionado página de projetos

--- my-app/src/app/projetos/page.jsx
+++ my-app/src/app/projetos/page.jsx
@@ -1,13 +1,40 @@
 import Menu from "@/components/Menu"
+import { meusProjetos } from "@/listProj/myProjects"
 
 export default function Projetos() {
     return (
-        <main className="max-w-[1024px] m-auto">
+        <div className="container mx-auto">
             <Menu />
-            <div className="w-full m-auto flex flex-col justify-center items-center gap-2">
-                <h1 className="text-blue-400 text-4xl">Essa página está em manutenção!</h1>
-                <p className="text-gray-50 opacity-80">Peço desculpas pelo incoveniente, mas no momento estou fazendo umas melhorias.</p>
-            </div>
-        </main>
+            <main className="">
+                <h1 className="text-blue-500 text-3xl mb-8">Projetos</h1>
+
+                <ul>
+                    {meusProjetos.map(projeto => (
+                        <li key={projeto.name} className=" border p-4 rounded text-teal-500">
+                            <h2 className="text-xl uppercase mb-4">{projeto.name}</h2>
+                            <p className="mb-3 text-white/72">
+                                {projeto.description}
+                            </p>
+                            <div className="flex gap-4">
+                                <a
+                                    href={projeto.urlGit}
+                                    target="_blank"
+                                    className="text-orange-300 text-sm font-medium capitalize border px-2 py-1 hover:text-teal-400 transition duration-300 ease-in-out"
+                                >
+                                    gitHub
+                                </a>
+                                <a
+                                    href={projeto.urlVercel}
+                                    target="_blank"
+                                    className="text-yellow-300 text-sm font-medium capitalize border px-2 py-1 hover:text-teal-400 transition duration-300 ease-in-out"
+                                >
+                                    vercel
+                                </a>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </main>
+        </div>
     )
 }
---
+++ my-app/src/listProj/myProjects.js
@@ -0,0 +1,8 @@
+export const meusProjetos = [
+    {
+        name: 'controle-clientes',
+        description: 'Feito com Next.js e Tailwind no Frontend, Node.js no backend e MongoDB como banco de dados.',
+        urlGit: 'https://github.com/WemersonNunesDev/controle-clientes',
+        urlVercel: 'https://controle-clientes-woad.vercel.app/',
+    },
+]
